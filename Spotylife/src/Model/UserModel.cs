8411df1264f5911d803866bdf5ded1aaff4e6e48
José Vinicius <vinicius123.vs58@gmail.com>
2022-07-02 02:30:00 -0300

    @Josevinicius53[Task: Crindo pasta src/tabela users e dependencias{02/07/2022}]

---
+++ Spotylife/src/Model/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Spotylife.src.Model
+{
+    [Table("Tb_users")]
+    public class UserModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+        
+        [Required, StringLength(100)]
+        public string Email { get; set; }
+
+        [Required, StringLength(100)]
+        public string Password { get; set; }
+
+        [Required,StringLength(100)]
+        public string Bio { get; set; }
+
+
+
+
+    }
+}
